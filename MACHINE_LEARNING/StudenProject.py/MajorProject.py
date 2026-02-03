7519913d39461b2b6662f40a15a7416f83bf9238
kumarHemant8897 <kumarhemantoffical2022@gmail.com>
2026-02-03 20:40:42 +0530

    Commit all changes

---
+++ MACHINE_LEARNING/StudenProject.py/MajorProject.py
@@ -0,0 +1,10 @@
+import matplotlib as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score
+
+
+df=pd.read_csv("\Student_simple.csv")
+
+x=df[[]]
