ae6d86cb14188d23da295c6554fc628cfd24bbb9
kumarHemant8897 <kumarhemantoffical2022@gmail.com>
2026-02-04 13:17:52 +0530

    Commit all changes

--- MACHINE_LEARNING/UNSUPERVISED/PCA.py
+++ MACHINE_LEARNING/UNSUPERVISED/PCA.py
@@ -4,16 +4,45 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error,mean_squared_error
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
 
 
 
 #somaple data
 data={
-    'cutomer':['riya','aman','fiazan','neha','imran','senha'],
-    'age':[20,30,40,42,38,25],
-    'spending':[100,200,300,110,290,130]
+    
+    'age':[25,30,35,40,45,50],
+    'spending':[70,60,50,40,30,20],
+    'income':[30000,40000,50000,60000,70000,80000],
+    'saving':[1000,5000,10000,15000,20000,6000]
 }
 
 df=pd.DataFrame(data)
 
+scaler=StandardScaler()
+scaled_data=scaler.fit_transform(df)
+
+pca=PCA(n_components=2)
+pca_result=pca.fit_transform(scaled_data)
+
+pca_df=pd.DataFrame(pca_result,columns=['PCA1','PCA2'])
+
+expalin_varience=pca.explained_variance_ratio_
+print("varience captured by each compent is :   ")
+print(np.round(expalin_varience*100,2))
+
+
+
+plt.figure(figsize=(8,6))
+plt.scatter(pca_df['PCA1'] ,pca_df['PCA2'],color='black',s=2)
+plt.title("PCA projection in (2d view)")
+plt.xlabel("PCA1 main patter ")
+plt.ylabel("PCA2 minor Pattern")
+plt.grid(True)
+
+plt.show()
+
+print("varinece captured by each compenent is : ")
+print(pca_df)
